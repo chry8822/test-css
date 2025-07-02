4bdaa397f2b3a008a0b7073767238e786f00d952
오국화 <okh8822@hmcnetworks.co.kr>
2025-07-02 12:55:26 +0900

    Update project files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:'/',
+  base: '/css-practice/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     host: '0.0.0.0',
     port: 3000,
